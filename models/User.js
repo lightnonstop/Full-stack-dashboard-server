d3c645a56303c9fb2c03288b4786acdf54bb28b0
lightnonstop <umichaeledet003@gmail.com>
2023-06-28 17:07:42 +0100

    add user model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+import mogoose from 'mongoose';
+
+const UserSchema = new mogoose.Schema({
+    name: { type: String, required: true, min: 2, max: 100, },
+    email: { type: String, required: true, unique: true, max: 50, },
+    password: { type: String, required: true, min: 5, },
+    city: String, 
+    state: String, 
+    country: String, 
+    occupation: String, 
+    phoneNumber: String,
+    transactions: Array,
+    role: {
+        type: String,
+        enum: ['user', 'admin', 'superadmin'],
+        default: 'admin',
+    },
+}, { timestamps: true, },)
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
