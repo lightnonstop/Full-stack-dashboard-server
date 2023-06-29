946d0d45c612efc5563b9d2386b5552e66e97ba7
lightnonstop <umichaeledet003@gmail.com>
2023-06-29 16:42:00 +0100

    control getting user information

---
+++ controllers/general.js
@@ -0,0 +1,13 @@
+import User from '../models/User.js'
+
+export const getUser = async (req, res) => {
+    try{
+        const { id } = req.params;
+        console.log("🚀 ~ file: general.js:6 ~ getUser ~ id:", id)
+        const user = await User.findById(id);
+        res.status(200).json(user);
+    } catch (error){
+        res.status(404).json({ message: error.message })
+    }
+}
+export default getUser;
