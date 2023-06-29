68f236f06336aa29b16bf17b05ea6927aa5c3f4b
lightnonstop <umichaeledet003@gmail.com>
2023-06-29 16:41:38 +0100

    add all necessary imports

--- index.js
+++ index.js
@@ -5,26 +5,31 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
 import helmet from 'helmet'
-// import clientRoutes from './routes/client.js'
-// import generalRoutes from './routes/general.js'
-// import managementRoutes from './routes/management.js'
-// import salesRoutes from './routes/sales.js'
+import clientRoutes from './routes/client.js'
+import generalRoutes from './routes/general.js'
+import managementRoutes from './routes/management.js'
+import salesRoutes from './routes/sales.js'
+
+//Data
+
+import User from './models/User.js'
+import { dataUser } from './data/index.js'
 
 /* CONFIGURATION */
 dotenv.config();
 const app = express();
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
-app.use(morgan('common'));
+app.use(morgan());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-// app.use('/client', clientRoutes)
-// app.use('/general', generalRoutes)
-// app.use('/management', managementRoutes)
-// app.use('/sales ', salesRoutes)
+app.use('/client', clientRoutes)
+app.use('/general', generalRoutes)
+app.use('/management', managementRoutes)
+app.use('/sales ', salesRoutes)
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 8080;
@@ -33,5 +38,6 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+    // User.insertMany(dataUser);
 })
 .catch((error) => console.log(`${error} did not connect`));
