ecb1c07c1990b13813489954edf9599e092d175a
lightnonstop <umichaeledet003@gmail.com>
2023-06-29 16:48:47 +0100

    fix product stat model name

--- models/ProductStat.js
+++ models/ProductStat.js
@@ -19,5 +19,5 @@ const ProductStatSchema = new mongoose.Schema({
     },
 }, { timestamps: true, });
 
-const User = mongoose.model('ProductStat', ProductStatSchema);
-export default Product;
+const ProductStat = mongoose.model('ProductStat', ProductStatSchema);
+export default ProductStat;
