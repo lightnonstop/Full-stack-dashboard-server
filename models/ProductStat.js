6c402a48a928d4799ac9e2da383d8483fde3e4e0
lightnonstop <umichaeledet003@gmail.com>
2023-06-29 16:41:01 +0100

    add product stat model

---
+++ models/ProductStat.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const ProductStatSchema = new mongoose.Schema({
+    productId: String,
+    yearlySalesTotal: Number,
+    yearlyTotalSoldUnits: Number,
+    year: Number,
+    monthlyData: [
+        {
+            month: String,
+            totalSales: Number,
+            totalUnits: Number,
+        },
+    ],
+    dailyData: {
+        date: String,
+        totalSales: Number,
+        totalUnits: Number,
+    },
+}, { timestamps: true, });
+
+const User = mongoose.model('Product', ProductStatSchema);
+export default Product;
