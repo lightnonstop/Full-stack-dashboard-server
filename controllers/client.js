c6871bd1c7985254162a0c99a5b86caef86ffb25
lightnonstop <umichaeledet003@gmail.com>
2023-06-30 15:35:49 +0100

    fix conflicting names: get transaction and get customers

--- controllers/client.js
+++ controllers/client.js
@@ -21,6 +21,40 @@ export const getProducts = async (req, res) => {
         res.status(404).json({ message: error.message })
     }
 }
+export const getTransactions = async (req, res) => {
+    try{
+        const { page = 1, pageSize = 20, sort = null, search = '' } = req.query;
+
+        const generateSort = () => {
+            const sortParsed = JSON.parse(sort);
+            const sortFormatted = {
+                [sortParsed.field]: (sortParsed.sort === 'asc' ? 1 : -1),
+            }
+            return sortFormatted;
+        }
+        const sortFormatted = Boolean(sort) ? generateSort() : {};
+        const transactions = await Transaction.find({
+            $or: [
+                { cost: { $regex: new RegExp(search, 'i') } },
+                { userId: { $regex: new RegExp(search, 'i') } },
+            ],
+        })
+        .sort(sortFormatted)
+        .skip(page * pageSize)
+        .limit(pageSize);
+
+        const total = await Transaction.countDocuments({
+            name: { $regex: search, $options: 'i' }
+        })
+
+        res.status(200).json({
+            transactions, total
+        })
+    } catch (error){
+        res.status(404).json({ message: error.message })
+    }
+}
+
 export const getCustomers = async (req, res) => {
     try{
         const customers = await User.find({ role: 'user' }).select('-password')
