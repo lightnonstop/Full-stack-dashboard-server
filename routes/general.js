379a251d8a3ab402d792541783c15c2edfc31464
lightnonstop <umichaeledet003@gmail.com>
2023-07-05 12:51:05 +0100

    add dashboard server route

--- routes/general.js
+++ routes/general.js
@@ -4,5 +4,6 @@ import { getUser } from '../controllers/general.js';
 const router = express.Router();
 
 router.get('/user/:id', getUser);
+router.get('/dashboard', getDashboardStats);
 
 export default router;
